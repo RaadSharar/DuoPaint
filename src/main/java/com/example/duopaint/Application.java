45ae08a243d4c4ea2ac8c9a617d62185ce92b5c8
Raad Sharar <raadthehero.rs@gmail.com>
2025-07-18 01:30:06 +0600

    doing the menu UI (all main scene part)

--- src/main/java/com/example/duopaint/Application.java
+++ src/main/java/com/example/duopaint/Application.java
@@ -1,11 +1,11 @@
 package com.example.duopaint;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,30 +13,30 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(Application.class.getResource("MainMenu.fxml"));
-        stage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        stage.setTitle("skribbl!");
         Scene scene = new Scene(root);
         String css = getClass().getResource("MainMenu.css").toExternalForm();
         scene.getStylesheets().add(css);
         stage.setScene(scene);
         stage.show();
-
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("favicon.png")));
         stage.setOnCloseRequest((e) -> {
             e.consume();
             try {
-                logout(stage);
+                quitgame(stage);
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
         });
     }
-    public void logout(Stage stage) throws IOException {
+    public void quitgame(Stage stage) throws IOException {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Logout");
-        alert.setHeaderText("Logout");
-        alert.setContentText("Are you sure you want to logout?");
-        if (alert.showAndWait().get() == ButtonType.OK) {
-            System.out.println("You closed the window");
+        alert.setTitle("Exit");
+        alert.setHeaderText("Quit Game?");
+        alert.setContentText("Are you sure you want to exit?");
+        alert.getButtonTypes().setAll(ButtonType.YES, ButtonType.NO);
+        if (alert.showAndWait().get() == ButtonType.YES) {
             stage.close();
         }
     }
